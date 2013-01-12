fa1545304a7181dbbca8adb8a86e53a94bbeb9dd
Memo Akten <memo@memo.tv>
2013-01-12 18:17:21 +0200

    example uses custom data

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -1,50 +1,71 @@
 #include "testApp.h"
 #include "ofxMSAOrderedPointerMap.h"
 
+
+// an arbitrary class to hold some data
+class PersonData {
+public:
+    float age;
+    float height;
+    
+    PersonData(float age, float height) {
+        this->age = age;
+        this->height = height;
+    }
+    
+    string toString() {
+        return "[ age: " + ofToString(age) + ", height: " + ofToString(height) + "cm ]";
+    }
+};
+
+
+// create an ordered named map of PersonData pointers
+msa::OrderedPointerMap<PersonData> a;
+
+
+// this will store our output
 stringstream outputStream;
+
 
 //--------------------------------------------------------------
 void testApp::setup() {
 
-    // Ordered pointer map of ofVec3f's
-    msa::OrderedPointerMap<ofVec3f> a;
-    
     
     // add some data
     outputStream << "adding data..." << endl;
-    a.push_back("memo", ofVec3f(37, 175, 70));  // age (years), height (cm), weight (kg)
-    a.push_back("jane", ofVec3f(27, 165, 55));
-    a.push_back("pearl", ofVec3f(2, 15, 0.52));
-    a.push_back("bruce", ofVec3f(3, 12, 0.45));
+    a.push_back("memo", PersonData(37, 175));  // age (years), height (cm), weight (kg)
+    a.push_back("jane", PersonData(27, 165));
+    a.push_back("pearl", PersonData(2, 15));
+    a.push_back("bruce", PersonData(3, 12));
     
-    outputStream << "memo's info: " << a["memo"] << endl;
-    outputStream << "jane's info: " << a["jane"] << endl;
-    outputStream << "pearl's info: " << a["pearl"] << endl;
-    outputStream << "bruce's info: " << a["bruce"] << endl;
+    outputStream << "memo - " << a["memo"].toString() << endl;
+    outputStream << "jane - " << a["jane"].toString() << endl;
+    outputStream << "pearl - " << a["pearl"].toString() << endl;
+    outputStream << "bruce - " << a["bruce"].toString() << endl;
     
     // i know this one doesn't exist, so checking first to avoid an exception
-    if(a.exists("blufo")) outputStream << "blufo's info: " << a["blufo"] << endl;
+    if(a.exists("blufo")) outputStream << "blufo - " << a["blufo"].toString() << endl;
     else outputStream << "blufo doesn't exist!" << endl;
     
     
     // accessing via name
     outputStream << endl << endl << "accessing and modifying via names (make 1 year older)..." << endl;
     // various different ways of modifying:
-    a["memo"].x = 38;
-    a["jane"].x = a["jane"].x + 1;
-    a["pearl"] = a["pearl"] + ofVec3f(1, 0, 0);
-    a["bruce"].x++;
-    outputStream << "memo's info: " << a["memo"] << endl;
-    outputStream << "jane's info: " << a["jane"] << endl;
-    outputStream << "pearl's info: " << a["pearl"] << endl;
-    outputStream << "bruce's info: " << a["bruce"] << endl;
+    a["memo"].age = 38;
+    a["jane"].age = a["jane"].age + 1;
+    a["pearl"].age += 1;
+    a["bruce"].age++;
+    outputStream << "memo - " << a["memo"].toString() << endl;
+    outputStream << "jane - " << a["jane"].toString() << endl;
+    outputStream << "pearl - " << a["pearl"].toString() << endl;
+    outputStream << "bruce - " << a["bruce"].toString() << endl;
 
     
     // accessing via index
     outputStream << endl << endl << "accessing and modifying via index (make 1 year older).." << endl;
     for(int i=0; i<a.size(); i++) {
-        a[i].x += 1;
-        outputStream << a.nameAt(i) << "'s info: " << a[i] << endl;
+        a[i].age ++;
+        outputStream << a.nameAt(i) << " - " << a[i].toString() << endl;
     }
     
 }
