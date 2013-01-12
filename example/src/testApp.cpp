141a47d6ba8e3fa3f6e6c6583873fa320a5024bc
Memo Akten <memo@memo.tv>
2013-01-12 18:09:42 +0200

    updated example

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -1,44 +1,50 @@
 #include "testApp.h"
 #include "ofxMSAOrderedPointerMap.h"
 
-stringstream str;
+stringstream outputStream;
 
 //--------------------------------------------------------------
 void testApp::setup() {
+
+    // Ordered pointer map of ofVec3f's
     msa::OrderedPointerMap<ofVec3f> a;
     
-    a.push_back("memo", ofVec3f(37, 175, 70));  // age, height, weight
-    a.push_back("jane", ofVec3f(27, 165, 60));
-    a.push_back("pearl", ofVec3f(2, 15, 55));
-    a.push_back("bruce", ofVec3f(3, 12, 48));
     
-    str << "memo: " << a["memo"] << endl;
-    str << "jane: " << a["jane"] << endl;
-    str << "pearl: " << a["pearl"] << endl;
-    str << "bruce: " << a["bruce"] << endl;
+    // add some data
+    outputStream << "adding data..." << endl;
+    a.push_back("memo", ofVec3f(37, 175, 70));  // age (years), height (cm), weight (kg)
+    a.push_back("jane", ofVec3f(27, 165, 55));
+    a.push_back("pearl", ofVec3f(2, 15, 0.52));
+    a.push_back("bruce", ofVec3f(3, 12, 0.45));
     
-    // i know this one doesn't exist, so checking first to avoid an assert
-    if(a.exists("blufo")) {
-        str << "blufo: " << a["blufo"] << endl;
-    } else {
-        str << "blufo: doesn't exist!" << endl;
-    }
+    outputStream << "memo's info: " << a["memo"] << endl;
+    outputStream << "jane's info: " << a["jane"] << endl;
+    outputStream << "pearl's info: " << a["pearl"] << endl;
+    outputStream << "bruce's info: " << a["bruce"] << endl;
+    
+    // i know this one doesn't exist, so checking first to avoid an exception
+    if(a.exists("blufo")) outputStream << "blufo's info: " << a["blufo"] << endl;
+    else outputStream << "blufo doesn't exist!" << endl;
     
     
-    str << endl << "modifying via names (1 year older)..." << endl;
+    // accessing via name
+    outputStream << endl << endl << "accessing and modifying via names (make 1 year older)..." << endl;
+    // various different ways of modifying:
     a["memo"].x = 38;
     a["jane"].x = a["jane"].x + 1;
     a["pearl"] = a["pearl"] + ofVec3f(1, 0, 0);
     a["bruce"].x++;
-    str << "memo: " << a["memo"] << endl;
-    str << "jane: " << a["jane"] << endl;
-    str << "pearl: " << a["pearl"] << endl;
-    str << "bruce: " << a["bruce"] << endl;
+    outputStream << "memo's info: " << a["memo"] << endl;
+    outputStream << "jane's info: " << a["jane"] << endl;
+    outputStream << "pearl's info: " << a["pearl"] << endl;
+    outputStream << "bruce's info: " << a["bruce"] << endl;
+
     
-    str << endl << "iterating and modifying via index (1 year older).." << endl;
+    // accessing via index
+    outputStream << endl << endl << "accessing and modifying via index (make 1 year older).." << endl;
     for(int i=0; i<a.size(); i++) {
         a[i].x += 1;
-        str << a.nameAt(i) << ": " << a[i] << endl;
+        outputStream << a.nameAt(i) << "'s info: " << a[i] << endl;
     }
     
 }
@@ -46,6 +52,6 @@ void testApp::setup() {
 //--------------------------------------------------------------
 void testApp::draw() {
     ofBackground(0);
-    ofDrawBitmapString(str.str(), 10, 30);
+    ofDrawBitmapString(outputStream.str(), 20, 30);
 }
 
